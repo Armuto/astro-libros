d2cbdfcd822fa4306c67fb702332bf7566e6a8a9
Armuto <devarmuto@gmail.com>
2023-07-18 14:33:05 -0500

    first commit

---
+++ src/data/books.js
@@ -0,0 +1,44 @@
+export const books=[
+    {
+        id:'clean-code',
+        title:'Clean Code: Manual de desarrollo ágil de software',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg",
+        opinion:"1",
+        author:"Robert C. Martin",
+    },
+    {
+        id:'the-clean-coder',
+        title:'The Clean Coder: A Code of Conduct for Professional Programmers',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg",
+        opinion:"2",
+        author:"Robert C. Martin",
+    },
+    {
+        id:'el-yerno-millonario',
+        title:'El yerno millonario: Carismatico Charlie Wade',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1610670003i/56670795.jpg",
+        opinion:"w3",
+        author:"Bryan Vasquez",
+    },
+    {
+        id:'como-ser-un-estoico',
+        title:'Cómo ser un estoico',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1537740024i/41976867.jpg",
+        opinion:"www",
+        author:"Massimo Pigliucci",
+    },
+    {
+        id:'javaScript-the-good-parts',
+        title:'JavaScript: The Good Parts',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg",
+        opinion:"asd",
+        author:"Douglas Crockford",
+    },
+    {
+        id:'eloquent-javaScript',
+        title:'Eloquent JavaScript: A Modern Introduction to Programming',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg",
+        opinion:"asd",
+        author:"Marijn Haverbeke",
+    }
+]
